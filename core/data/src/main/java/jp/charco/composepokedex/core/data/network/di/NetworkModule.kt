962ce7ffbaec2a1cfcd9076024410bdd73e889f4
charcoJP <charco2356@gmail.com>
2023-05-28 11:59:51 +0900

    :sparkles: network のDI設定

---
+++ core/data/src/main/java/jp/charco/composepokedex/core/data/network/di/NetworkModule.kt
@@ -0,0 +1,48 @@
+package jp.charco.composepokedex.core.data.network.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import jp.charco.composepokedex.core.data.network.PokeApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }
+            ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://pokeapi.co/api/v2/")
+            .callFactory(okHttpClient)
+            .addConverterFactory(
+                Json.asConverterFactory("application/json".toMediaType())
+            )
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providePokeApi(retrofit: Retrofit): PokeApi {
+        return retrofit.create(PokeApi::class.java)
+    }
+}
