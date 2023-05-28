8cebbf57efa9182c9711f0aebdcf60153f9177c9
charcoJP <charco2356@gmail.com>
2023-05-29 01:11:46 +0900

    :sparkles: PokemonsScreen を実装

--- app/src/main/java/jp/charco/composepokedex/MainActivity.kt
+++ app/src/main/java/jp/charco/composepokedex/MainActivity.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
+import jp.charco.composepokedex.feature.pokemons.PokemonsScreen
 import jp.charco.composepokedex.ui.theme.ComposePokedexTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,10 +25,11 @@ class MainActivity : ComponentActivity() {
             ComposePokedexTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Scaffold(topBar = { TopBar() }) { contentPadding ->
+                        PokemonsScreen(contentPadding = contentPadding)
+                    }
                 }
             }
         }
@@ -30,17 +37,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+@OptIn(ExperimentalMaterial3Api::class)
+fun TopBar() {
+    CenterAlignedTopAppBar(title = { Text("ComposePokedex") })
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposePokedexTheme {
-        Greeting("Android")
     }
 }
---
+++ feature/pokemons/src/main/java/jp/charco/composepokedex/feature/pokemons/PokemonsScreen.kt
@@ -0,0 +1,55 @@
+package jp.charco.composepokedex.feature.pokemons
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun PokemonsScreen(
+    contentPadding: PaddingValues,
+    viewModel: PokemonsViewModel = hiltViewModel(),
+) {
+    val uiState by viewModel.uiStateFlow.collectAsStateWithLifecycle()
+    Contents(uiState, contentPadding)
+}
+
+@Composable
+fun Contents(uiState: MainUiState, contentPadding: PaddingValues) {
+    when (uiState) {
+        is MainUiState.Error -> Text("エラー")
+        MainUiState.Loading -> CircularProgressIndicator()
+        is MainUiState.Success -> {
+            LazyColumn(modifier = Modifier.fillMaxSize().padding(contentPadding)) {
+                items(uiState.pokemonList) {
+                    Card(
+                        shape = RoundedCornerShape(16.dp),
+                        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface),
+                    ) {
+                        Text(
+                            text = it.name,
+                            modifier = Modifier.padding(
+                                horizontal = 16.dp,
+                                vertical = 24.dp
+                            )
+                        )
+                    }
+
+                }
+            }
+        }
+    }
+}
