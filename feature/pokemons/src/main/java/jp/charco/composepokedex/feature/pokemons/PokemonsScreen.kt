4a31c8b9043dd845bff86669ff28df157ee6e74b
charcoJP <charco2356@gmail.com>
2023-05-30 00:02:21 +0900

    :lipstick: 画像からPaletteで色を抽出して背景色にする

--- feature/pokemons/src/main/java/jp/charco/composepokedex/feature/pokemons/PokemonsScreen.kt
+++ feature/pokemons/src/main/java/jp/charco/composepokedex/feature/pokemons/PokemonsScreen.kt
@@ -1,27 +1,40 @@
 package jp.charco.composepokedex.feature.pokemons
 
+import android.graphics.Bitmap
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.core.graphics.drawable.toBitmap
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import coil.compose.AsyncImage
+import androidx.palette.graphics.Palette
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
 import jp.charco.composepokedex.core.data.network.response.Pokemon
 
 @Composable
@@ -54,8 +67,11 @@ fun Contents(uiState: MainUiState, contentPadding: PaddingValues) {
 
 @Composable
 private fun PokemonRow(pokemon: Pokemon) {
-    Card(
-        shape = RoundedCornerShape(16.dp),
+    var bitmap: Bitmap? by remember { mutableStateOf(null) }
+    val palette = remember(bitmap) { bitmap?.let { Palette.from(it).generate() } }
+    val cardBackgroundColor = palette?.dominantSwatch?.rgb?.let { Color(it) } ?: Color.White
+
+    OutlinedCard(
         colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface),
         modifier = Modifier
             .fillMaxSize()
@@ -65,14 +81,32 @@ private fun PokemonRow(pokemon: Pokemon) {
             )
     ) {
         Row {
-            AsyncImage(
-                model = pokemon.imageUrl,
-                contentDescription = pokemon.name,
-                modifier = Modifier
-                    .height(100.dp)
-                    .padding(end = 16.dp)
+            val painter = rememberAsyncImagePainter(
+                ImageRequest.Builder(LocalContext.current)
+                    .data(data = pokemon.imageUrl)
+                    .allowHardware(false)
+                    .listener(
+                        onSuccess = { _, result ->
+                            bitmap = result.drawable.toBitmap()
+                        }
+                    )
+                    .build()
             )
-            Column {
+            Image(
+                painter = painter,
+                contentDescription = "",
+                modifier = Modifier
+                    .background(color = cardBackgroundColor)
+                    .height(100.dp)
+                    .width(100.dp)
+                    .padding(8.dp)
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp)
+                    .align(Alignment.CenterVertically),
+            ) {
                 Text(text = "No.${pokemon.number}", style = MaterialTheme.typography.labelMedium)
                 Spacer(modifier = Modifier.height(4.dp))
                 Text(text = pokemon.name, style = MaterialTheme.typography.headlineMedium)
