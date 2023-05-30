6737f0149993d426af9d58635255979df5ed0faa
charcoJP <charco2356@gmail.com>
2023-05-31 00:28:08 +0900

    :heavy_plus_sign: Add compose ui google fonts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,7 @@ dependencies {
 
     implementation(platform(libs.compose.bom))
     implementation(libs.compose.ui)
+    implementation(libs.compose.ui.text.google.fonts)
     implementation(libs.compose.ui.graphics)
     implementation(libs.compose.ui.tooling.preview)
     implementation(libs.compose.material3)
