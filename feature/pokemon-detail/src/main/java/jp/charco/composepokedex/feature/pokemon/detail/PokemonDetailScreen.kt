c7561445a9b37048d448a8c4bc13987cbdadcc78
charcoJP <charco2356@gmail.com>
2023-06-11 23:53:06 +0900

    :lipstick: 詳細に名前、画像などを一旦表示

--- feature/pokemon-detail/src/main/java/jp/charco/composepokedex/feature/pokemon/detail/PokemonDetailScreen.kt
+++ feature/pokemon-detail/src/main/java/jp/charco/composepokedex/feature/pokemon/detail/PokemonDetailScreen.kt
@@ -1,12 +1,56 @@
 package jp.charco.composepokedex.feature.pokemon.detail
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil.compose.AsyncImage
+import jp.charco.composepokedex.core.ui.theme.fontFamily
+import jp.charco.composepokedex.feature.pokemon.detail.PokemonDetailViewModel.PokemonDetailUiState.Error
+import jp.charco.composepokedex.feature.pokemon.detail.PokemonDetailViewModel.PokemonDetailUiState.Loading
+import jp.charco.composepokedex.feature.pokemon.detail.PokemonDetailViewModel.PokemonDetailUiState.Success
 
 @Composable
 fun PokemonDetailScreen(
     viewModel: PokemonDetailViewModel = hiltViewModel(),
 ) {
-    Text("詳細だよ: ${viewModel.pokemonNumber}")
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    when (uiState) {
+        is Error -> Text("Error: ${(uiState as Error).exception?.localizedMessage}")
+        Loading -> CircularProgressIndicator()
+        is Success -> {
+            val pokemon = (uiState as Success).pokemon
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                Text(
+                    pokemon.name,
+                    style = MaterialTheme.typography.displayMedium,
+                    fontFamily = fontFamily
+                )
+                AsyncImage(
+                    model = pokemon.imageUrl,
+                    contentDescription = "",
+                    modifier = Modifier
+                        .padding(8.dp)
+                )
+                Text("height: ${pokemon.height}")
+                Text("weight: ${pokemon.weight}")
+                Text("weight: ${pokemon.types.joinToString { it.type.name }}")
+            }
+        }
+    }
 }
--- feature/pokemon-detail/src/main/java/jp/charco/composepokedex/feature/pokemon/detail/PokemonDetailViewModel.kt
+++ feature/pokemon-detail/src/main/java/jp/charco/composepokedex/feature/pokemon/detail/PokemonDetailViewModel.kt
@@ -2,18 +2,44 @@ package jp.charco.composepokedex.feature.pokemon.detail
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import jp.charco.composepokedex.core.data.PokeRepository
+import jp.charco.composepokedex.core.data.network.response.PokemonDetail
 import jp.charco.composepokedex.feature.pokemon.detail.navigation.pokemonNumberArg
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class PokemonDetailViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
+    pokeRepository: PokeRepository
 ) : ViewModel() {
 
-    val pokemonNumber: String
+    private val pokemonNumber: String
+
+    private val _uiState = MutableStateFlow<PokemonDetailUiState>(PokemonDetailUiState.Loading)
+    val uiState = _uiState.asStateFlow()
 
     init {
         pokemonNumber = savedStateHandle[pokemonNumberArg] ?: throw IllegalArgumentException()
+
+        viewModelScope.launch {
+            runCatching {
+                val result = pokeRepository.getPokemonDetail(pokemonNumber)
+                _uiState.update { PokemonDetailUiState.Success(result) }
+            }.onFailure { e ->
+                _uiState.update { PokemonDetailUiState.Error(e) }
+            }
+        }
+    }
+
+    sealed interface PokemonDetailUiState {
+        data class Success(val pokemon: PokemonDetail) : PokemonDetailUiState
+        data class Error(val exception: Throwable?) : PokemonDetailUiState
+        object Loading : PokemonDetailUiState
     }
 }
